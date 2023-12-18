9ce64f2fa4ef4ca8e15efa1933c45d4a89be8b50
peter_strong <p.r.strong@warwick.ac.uk>
2023-12-18 18:26:33 +0000

    Neaten code and fixed dataset issues

--- BMA_example.py
+++ BMA_example.py
@@ -9,18 +9,22 @@ import matplotlib.pyplot as plt
 
 
 # create event tree
-data = pd.read_csv("datasets_test3/Titanic.csv")
+data = pd.read_csv("datasets/Falls_Data.csv")
+#to test on smaller dataset try
+# data = pd.read_csv("datasets/Falls_Data_10000.csv")
 show_event_tree(data)
+
 
 # %%
 
 # create stage tree
-st = staged.StagedTree(data,struct_missing_label= "nan_3")
+st = staged.StagedTree(data)
+#for any stuctured missing data use the optional argument: struct_missing_label= ""
 hyperstage = st._create_default_hyperstage()
 HAC_st = copy.deepcopy(st)
 #%%
 # run HAC and show CEG of output 
-HAC_st.calculate_AHC_transitions( alpha=4, hyperstage = hyperstage)
+HAC_st.calculate_AHC_transitions(alpha=4, hyperstage = hyperstage)
 HAC_CEG = ceg.ChainEventGraph(HAC_st)
 HAC_CEG.create_figure()
 
@@ -34,16 +38,16 @@ staging_output, model_weights,loglikelihoods = Bayesian_model_averaging_CEGs(st,
 for h in range(len(staging_output)):
     if len(model_weights[h])>1:
         labels=[str(x) for x in staging_output[h]]
-        data = model_weights[h].flatten()  # Flatten the array to 1D
+        data = model_weights[h].flatten() 
 
-        plt.pie(data, labels=labels)#,autopct = lambda p:round(p+0.07)/100,startangle=0,radius =1.25)
+        plt.pie(data, labels=labels)
         plt.title( "Normalised Bayes factor ratios for hyperset\n"+ str(hyperstage[h]))        
         plt.show()
 #%%
 for h in range(len(staging_output)):
     if len(model_weights[h])>1:
         labels=[str(x) for x in staging_output[h]]
-        data = model_weights[h].flatten()  # Flatten the array to 1D
+        data = model_weights[h].flatten()  
 
         order=sorted(range(len(data)), key=data.__getitem__,reverse=True)
         ordered_labs=[labels[i] for i in order] 
@@ -60,8 +64,7 @@ for h in range(len(staging_output)):
 full_staging_output, full_model_weights = get_full_model_weights(model_weights, staging_output)
 # plot model weights for the well performing models
 y_pos = np.arange(len(full_staging_output))
-if len(full_staging_output[h])>1:
-    # full_model_weights = full_model_weights.flatten()
+if len(full_staging_output)>1:
     sorted_model_weights = sorted(full_model_weights,reverse=True)
 else:
     sorted_model_weights = full_model_weights
@@ -69,9 +72,7 @@ labels=["M"+str(i+1) for i in range(0,len(full_staging_output))]
 plt.bar(labels,sorted_model_weights , align='center', alpha=0.5)
 plt.ylabel('Model weights')
 plt.xticks(rotation=90)
-# plt.savefig('BMA_full_falls_weights.pdf', format='pdf', dpi=1000)
 plt.show()
-#save figure as pdf
 
 #%%
 # calculate well performing unions and intersections
@@ -83,8 +84,6 @@ for sets in list(intersection_staging.values()):
     intersection_set += sets
 for sets in list(union_staging.values()):
     union_set += sets
-#%%
-# save figures in high dpi
 # %%
 # show ceg of intersection set
 coarsest=copy.deepcopy(st)
@@ -102,3 +101,5 @@ coarsest.create_figure()
 
 coarsest_ceg=ceg.ChainEventGraph(coarsest)
 coarsest_ceg.create_figure()
+
+# %%
